4781c2c5683b9a29ad87e925ab5e7105b5a76078
Alexandra Ramirez <alexa090114@gmail.com>
2026-04-29 18:21:48 -0500

    "feat: implementar función agregarEdad para añadir edades y renderizar lista izquierda"

--- arreglos1.js
+++ arreglos1.js
@@ -1,2 +1,12 @@
 let arreglosIzquierda = [];
 let arreglosDerecha = [];
+
+function agregarEdad(){
+    let cmpEdad;
+    let edad;
+    cmpEdad = document.getElementById("edad");
+    edad = parseInt("cmpEdad");
+
+    arreglosIzquierda.push(edad);
+    pintarArregloIzquierdo();
+}
