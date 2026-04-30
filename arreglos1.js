f353a1a68aee544008d262b612d5e6c75f9c43d6
Alexandra Ramirez <alexa090114@gmail.com>
2026-04-29 20:11:15 -0500

    "feat: crear función agregarEdad"

--- arreglos1.js
+++ arreglos1.js
@@ -1,2 +1,13 @@
 let edadesIzquierdo = [];
 let edadesDerecho = [];
+
+function agregarEdad(){
+    let valor = document.getElementById("edad").value;
+    // busca el input con id="edad" y lee lo que el usuario escribe 
+    let numero = parseInt(valor);
+    // Convierte "12" (texto) 12 (numero)
+    edadesIzquierdo.push(valor);
+    //agregar ese número al final del arreglo izquierdo 
+    pintarArregloIzquierda();
+    // actualiza la tabla en pantalla (lo hacemos en el punto 3)
+} 
